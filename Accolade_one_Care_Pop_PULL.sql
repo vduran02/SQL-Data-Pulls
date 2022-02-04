fddab929e5701f20e2dc3bf6eb5f23ed14fbcf72
Valeria Duran <87989617+vduran02@users.noreply.github.com>
2022-02-04 11:19:45 -0600

    Create Accolade_one_Care_Pop_PULL.sql

---
+++ Accolade_one_Care_Pop_PULL.sql
@@ -0,0 +1,14 @@
+------ UAL Accolade One Population
+SELECT DISTINCT dw_member_id FROM stage1_acl_una_extract.member WHERE
+          udf16 = 'In-Scope' AND
+          udf21 IN ('IAH', 'NHC', 'HSC', 'HOU', 'HQS', 'HQJ') AND
+          NOT (udf23 ilike 'pilot')
+        and terminationdate >= '2021-12-31'
+and memberage >= 18
+
+
+
+-------Metasource Accolade One Population
+SELECT DISTINCT dw_member_id FROM stage1_acl_msr_extract.member
+WHERE terminationdate >= '2021-12-31'
+    and memberage >= 18
