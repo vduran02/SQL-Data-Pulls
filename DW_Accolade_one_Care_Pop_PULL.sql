5dcc7148bf1a0c2b846f922e1cad3b72d1bc11e8
Valeria Duran <87989617+vduran02@users.noreply.github.com>
2022-02-04 15:23:04 -0600

    Update and rename Accolade_one_Care_Pop_PULL.sql to DW_Accolade_one_Care_Pop_PULL.sql

--- DW_Accolade_one_Care_Pop_PULL.sql
+++ DW_Accolade_one_Care_Pop_PULL.sql
@@ -4,11 +4,12 @@ SELECT DISTINCT dw_member_id FROM stage1_acl_una_extract.member WHERE
           udf21 IN ('IAH', 'NHC', 'HSC', 'HOU', 'HQS', 'HQJ') AND
           NOT (udf23 ilike 'pilot')
         and terminationdate >= '2021-12-31'
-and memberage >= 18
+        and (udf17 not ilike '%COBRA%' and udf17 not ilike '%Retiree%')
+--and memberage >= 18
 
 
 
 -------Metasource Accolade One Population
 SELECT DISTINCT dw_member_id FROM stage1_acl_msr_extract.member
 WHERE terminationdate >= '2021-12-31'
-    and memberage >= 18
+    ---and memberage >= 18
