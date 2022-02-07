259e843435ce04874004b54b45b37e7f6be19fb0
Valeria Duran <87989617+vduran02@users.noreply.github.com>
2022-02-07 09:34:08 -0600

    Update DW_Accolade_one_Care_Pop_PULL.sql

--- DW_Accolade_one_Care_Pop_PULL.sql
+++ DW_Accolade_one_Care_Pop_PULL.sql
@@ -2,10 +2,11 @@
 SELECT DISTINCT dw_member_id FROM stage1_acl_una_extract.member WHERE
           udf16 = 'In-Scope' AND
           udf21 IN ('IAH', 'NHC', 'HSC', 'HOU', 'HQS', 'HQJ') AND
-          NOT (udf23 ilike 'pilot')
-        and terminationdate >= '2021-12-31'
+          udf23 not ilike '%pilot%' AND
+        udf17 ilike 'Active'
         and (udf17 not ilike '%COBRA%' and udf17 not ilike '%Retiree%')
---and memberage >= 18
+        and (currentstatus = 'Active' or (currentstatus = 'Termed'
+        and terminationdate >= '2022-01-01'))
 
 
 
